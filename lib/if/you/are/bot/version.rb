243c78f8f88f797a2f38c3cf46d75eaa1f96f6ce
Yumainaura <yuma.inaura@gmail.com>
2018-08-13 08:40:25 +0900

    bundle gem

---
+++ lib/if/you/are/bot/version.rb
@@ -0,0 +1,9 @@
+module If
+  module You
+    module Are
+      module Bot
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
---
+++ spec/if/you/are/bot_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe If::You::Are::Bot do
+  it "has a version number" do
+    expect(If::You::Are::Bot::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
